12d3a14173a2710f93950f55bbff99dee322b9f4
faaust <link.tank.r@gmail.com>
2015-06-30 13:01:59 +0200

    removed unneeded annotation

--- src/main/java/hu/ksrichard/Application.java
+++ src/main/java/hu/ksrichard/Application.java
@@ -2,13 +2,12 @@ package hu.ksrichard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Static entry point for Spring Boot application
+ * @author ksrichard
  */
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = {"hu.ksrichard.repo"})
 public class Application {
 
     public static void main(String[] args){
